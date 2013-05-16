3ebccb5e04dffe707be6cd03e90ca5c3e6f0d915
Jade Shan <evokeralucard@gmail.com>
2013-05-16 19:24:22 +0800

    update bean copy

--- Jade-Utils-base/src/main/java/jadeutils/reflect/BeanUtils.java
+++ Jade-Utils-base/src/main/java/jadeutils/reflect/BeanUtils.java
@@ -1,6 +1,11 @@
 package jadeutils.reflect;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang.StringUtils;
 
 public class BeanUtils {
 
@@ -33,6 +38,14 @@ public class BeanUtils {
 		setter.invoke(tagBean, getter.invoke(srcBean));
 	}
 
+	private static String getPropLabel(String methodName) {
+		String result = "";
+		if (methodName.startsWith("set") || methodName.startsWith("get")) {
+			result = methodName.substring(3);
+		}
+		return result;
+	}
+
 	/**
 	 * 复制bean对象的值
 	 * 
@@ -44,12 +57,29 @@ public class BeanUtils {
 	 */
 	public static void copyBean(Object srcBean, Object tagBean)
 			throws Exception {
-		Method[] mthsTarget = tagBean.getClass().getMethods();
-		for (Method setter : mthsTarget) {
-			if (setter.getName().startsWith("set")) {
-				String name = "get" + setter.getName().substring(3);
-				Method getter = srcBean.getClass().getDeclaredMethod(name);
-				setter.invoke(tagBean, getter.invoke(srcBean));
+		Map<String, Method> getterMap = new HashMap<String, Method>();
+		for (Method m : srcBean.getClass().getMethods()) {
+			String name = getPropLabel(m.getName());
+			if (StringUtils.isNotBlank(name)) {
+				getterMap.put(name, m);
+			}
+		}
+		Map<String, Method> setterMap = new HashMap<String, Method>();
+		for (Method m : tagBean.getClass().getMethods()) {
+			String name = m.getName();
+			if (StringUtils.isNotBlank(name)) {
+				setterMap.put(name, m);
+			}
+		}
+		for (Entry<String, Method> t : setterMap.entrySet()) {
+			Method setter = t.getValue();
+			Method getter = getterMap.get(t.getKey());
+			if (null != getter) {
+				try {
+					setter.invoke(tagBean, getter.invoke(srcBean));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
