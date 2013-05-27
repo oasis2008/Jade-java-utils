2c64333afce14e19fd42040b1cfab7951896abff
Jade Shan <evokeralucard@gmail.com>
2013-05-28 00:57:30 +0800

    update extJs Config

--- Jade-Utils-extJs/src/main/java/jadeutils/extjs/ModelCreater.java
+++ Jade-Utils-extJs/src/main/java/jadeutils/extjs/ModelCreater.java
@@ -3,11 +3,17 @@
  */
 package jadeutils.extjs;
 
+import jadeutils.message.MessageMap;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringUtils;
 
 /**
  * @author SHAN013
@@ -15,14 +21,31 @@ import java.util.List;
  */
 public class ModelCreater {
 
-	public static List<String[]> loadModel(String fileName) {
+	public static Pattern FIELD_PATTERN = Pattern.compile("(\\w)+(\\w)+");
+
+	public static void printConfigForExtJS(String modelName) {
+		List<String[]> fieldList = loadModel(modelName);
+		String text = genFields(modelName, fieldList);
+		System.out.println(text);
+	}
+
+	public static List<String[]> loadModel(String modelName) {
 		List<String[]> fieldList = new ArrayList<>();
 		BufferedReader br = null;
 		try {
-			br = new BufferedReader(new FileReader(fileName));
+			br = new BufferedReader(new FileReader(modelName));
 			String line = br.readLine();// 一次读入一行，直到读入null为文件结束
 			while (line != null) {
-				System.out.println(line);
+				Matcher m = FIELD_PATTERN.matcher(line);
+				String[] strArr = new String[m.groupCount()];
+				for (int i = 0; i < m.groupCount(); i++) {
+					if (m.find()) {
+						strArr[i] = m.group();
+					}
+				}
+				if (StringUtils.isNotBlank(strArr[0])) {
+					fieldList.add(strArr);
+				}
 				line = br.readLine(); // 接着读下一行
 			}
 		} catch (IOException e) {
@@ -37,5 +60,70 @@ public class ModelCreater {
 			}
 		}
 		return fieldList;
+	}
+
+	public static String genFields(String modelName, List<String[]> fieldList) {
+		StringBuffer sb = new StringBuffer();
+		/* */
+		sb.append("fields:[\n");
+		MessageMap mm = new MessageMap("modelFields");
+		for (int i = 0; i < fieldList.size(); i++) {
+			String[] f = fieldList.get(i);
+			if (null != f && f.length > 1 && StringUtils.isNotBlank(f[0])//
+					&& StringUtils.isNotBlank(f[1])) {
+				String line = mm.getMessage(f[0]);
+				line = line.replaceAll("\\{0\\}", f[1]);
+				if (StringUtils.isNotBlank(line)) {
+					sb.append("\t").append(line);
+					if (i < fieldList.size() - 1) {
+						sb.append(",");
+					}
+					sb.append("\n");
+				}
+			}
+		}
+		sb.append("]\n");
+		/* */
+		sb.append("fields:[\n");
+		sb.append("\t{xtype: 'rownumberer', text: messages['common.table.seq'], width: 15, sortable: false}");
+		mm = new MessageMap("tableFields");
+		for (int i = 0; i < fieldList.size(); i++) {
+			String[] f = fieldList.get(i);
+			if (null != f && f.length > 1 && StringUtils.isNotBlank(f[0])//
+					&& StringUtils.isNotBlank(f[1])) {
+				String line = mm.getMessage(f[0]);
+				line = line.replaceAll("\\{0\\}", modelName);
+				line = line.replaceAll("\\{1\\}", f[1]);
+				if (StringUtils.isNotBlank(line)) {
+					sb.append("\t").append(line);
+					if (i < fieldList.size() - 1) {
+						sb.append(",");
+					}
+					sb.append("\n");
+				}
+			}
+		}
+		sb.append("]\n");
+		/* */
+		/* */
+		sb.append("filterItems:[\n");
+		mm = new MessageMap("tableFilter");
+		for (int i = 0; i < fieldList.size(); i++) {
+			String[] f = fieldList.get(i);
+			if (null != f && f.length > 1 && StringUtils.isNotBlank(f[0])//
+					&& StringUtils.isNotBlank(f[1])) {
+				String line = mm.getMessage(f[0]);
+				line = line.replaceAll("\\{0\\}", f[1]);
+				if (StringUtils.isNotBlank(line)) {
+					sb.append("\t").append(line);
+					if (i < fieldList.size() - 1) {
+						sb.append(",");
+					}
+					sb.append("\n");
+				}
+			}
+		}
+		sb.append("]\n");
+		return sb.toString();
 	}
 }
---
+++ Jade-Utils-extJs/src/test/java/jadeutils/extjs/ExtJsTest.java
@@ -0,0 +1,12 @@
+package jadeutils.extjs;
+
+import org.junit.Test;
+
+public class ExtJsTest {
+
+	@Test
+	public void test() {
+		ModelCreater.printConfigForExtJS("user");
+	}
+
+}
